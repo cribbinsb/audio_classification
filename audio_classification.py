50652a90e7e9bc1f5763a5a8b512149ad5d14a1b
Bernando Cribbenza <bernandocribbenza@gmail.com>
2025-03-01 19:15:29 +0000

    onnx support

--- audio_classification.py
+++ audio_classification.py
@@ -5,21 +5,54 @@ import sounddevice as sd
 import librosa
 import torchaudio
 from panns_inference import SoundEventDetection, labels
+import onnx
+import onnxruntime as ort
 
-# Load PANNs model
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-print(f"device {device}")
-sed = SoundEventDetection(checkpoint_path=None, device=device, interpolate_mode='nearest')
-                                         
 # Audio settings
 SAMPLE_RATE = 32000  # Model expects 32kHz audio
-DURATION = 1       # Process 1 second of audio at a time
+DURATION = 0.5      # Process 1 second of audio at a time
 BUFFER_SIZE = int(SAMPLE_RATE * DURATION)
-        
-def classify_audio(audio_data):
-    framewise_output = sed.inference(audio_data)
-    framewise_output = framewise_output[0]
 
+model=None
+
+def load_model(use_onnx):
+    global model
+    if use_onnx==False:
+        # Load PANNs model
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        print(f"device {device}")
+        sed = SoundEventDetection(checkpoint_path=None, device=device, interpolate_mode='nearest')
+        model={"sed":sed}  
+    else:
+        onnx_path = "/mldata/weights/panns_model_opt.onnx"
+        session_options = ort.SessionOptions()
+        session_options.graph_optimization_level = ort.GraphOptimizationLevel.ORT_ENABLE_ALL
+        provider= "CUDAExecutionProvider" if torch.cuda.is_available() else "CPUExecutionProvider"
+        session = ort.InferenceSession(onnx_path, session_options, providers=[provider])
+        model={"session":session}
+                            
+def classify_audio(audio_data):
+    global model
+    if "sed" in model:
+        sed=model["sed"]
+        framewise_output = sed.inference(audio_data)
+        framewise_output = framewise_output[0]
+        print(framewise_output.shape) 
+    else:
+        session=model["session"]
+        # audio data needs to be some amount of 32Khz audio
+        # Nx320 sample (10ms chunks long)
+        # returned data is 527 probabilities (or however many classes there are)
+        # for each 10ms chunk
+        inputs = {"audio": audio_data}
+        outputs = session.run(["framewise_output"], inputs)
+
+        # Extract the output
+        framewise_output = outputs[0] 
+        framewise_output = np.squeeze(framewise_output, axis=0) # remove unneeded batch output
+        print(framewise_output.shape) 
+
+    # get top 5 classes
     classwise_output = np.max(framewise_output, axis=0) 
     idxes = np.argsort(classwise_output)[::-1]
     idxes = idxes[0:5]
@@ -29,13 +62,13 @@ def classify_audio(audio_data):
     for idx in idxes:
         # the array here is confidence vs time, for now just use the largest confidence that occurs
         largest=max(framewise_output[:, idx])
-        if largest>0.2:
+        if largest>0.01:
             print(idx, largest, ix_to_lb[idx])
 
 def process_microphone():
     """Captures live audio from the microphone and classifies it in real-time."""
     def callback(indata, frames, time, status):
-        print(f"callback {frames}")
+        
         if status:
             print(status)
         
@@ -51,12 +84,14 @@ def process_microphone():
         # Ensure proper shape
         audio_data = np.expand_dims(audio_data, axis=0)
 
+        print(f"callback {frames} SR {orig_sr} {audio_data.size}")
         # Predict labels
         classify_audio(audio_data)
 
 
     print("Listening for real-time classification... Press Ctrl+C to stop.")
-    with sd.InputStream(callback=callback, samplerate=48000, channels=1, blocksize=BUFFER_SIZE):
+    orig_sr = sd.query_devices(sd.default.device[0], 'input')['default_samplerate']
+    with sd.InputStream(callback=callback, samplerate=orig_sr, channels=1, blocksize=int(orig_sr*DURATION)):
         input()
 
 def process_wav_file(wav_file):
@@ -71,10 +106,14 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Classify audio using PANNs in real-time (microphone) or from a WAV file.")
     parser.add_argument("--mic", action="store_true", help="Use microphone for real-time classification")
     parser.add_argument("--wav", type=str, help="Path to WAV file for classification")
-    print("labels\n\n")
+    parser.add_argument('--onnx', action='store_true', help='use ONNX model')
+
+    print(f"{len(labels)} labels\n\n")
     print(labels)
     print("\n\n")
     args = parser.parse_args()
+
+    load_model(args.onnx)
     
     if args.mic:
         print(sd.query_devices())
